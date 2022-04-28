084325dfa64b423dbb4fd19a738c6af082b9f393
Addison McCabe <addisonhartmccabe@gmail.com>
2022-04-27 17:30:53 -0700

    updated commit

--- index.js
+++ index.js
@@ -2,22 +2,22 @@ console.log('Hello World')
 
 function addMovie(event) {
     event.preventDefault()
-    let inputField = document.querySelector("input");
+    let inputField = document.querySelector('input');
 
-let movie = document.createElement(`li`);
-let movieTitle = document.createElement(`span`)
+let movie = document.createElement('li');
+let movieTitle = document.createElement('span')
 movieTitle.textContent = inputField.value
-movieTitle.addEventListener(`click`, crossOffMovie)
+movieTitle.addEventListener('click', crossOffMovie)
 
 movie.appendChild(movieTitle)
-document.querySelector(`ul`).appendChild(movie)
+document.querySelector('ul').appendChild(movie)
 
 let deleteBtn = document.createElement('button')
 deleteBtn.textContent = 'x'
-deleteBtn.addEventListener(`click`, deleteMovie)
+deleteBtn.addEventListener('click', deleteMovie)
 movie.appendChild(deleteBtn)
 
-inputField.vaule = ``
+inputField.vaule = ''
 
-  return document.querySelector(`ul`).appendChild(movie)
+  return document.querySelector('ul').appendChild(movie)
 }
